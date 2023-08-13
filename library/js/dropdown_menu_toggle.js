3b36cc8d63730e4d02f197e7354dde1e01fa1eee
Svetlana Vorokhobina <svetlana.vorokhobina@gmail.com>
2023-08-14 11:01:52 +1200

    feat: add arrows in section About; update dropdown features in header

--- library/js/dropdown_menu_toggle.js
+++ library/js/dropdown_menu_toggle.js
@@ -10,15 +10,13 @@ function toggleMenu() {
         document.getElementById("header_dropdown-menu_icon").style.margin = "5px 0px auto 0px";
     }
 }
-  
+
 /* To close the menu, if we click outside of the button (class="header_dropdown-menu_button") */
-/* window.onclick = function(event) {
-    if (event.target.matches(".header_dropdown-menu_button") != true) {
-        let x = document.getElementsByClassName("header_dropdown-menu_content")[0];
-        if (x.classList.contains("showMenu") == true) {
-            x.classList.remove("showMenu");
-             document.getElementById("header_dropdown-menu_icon").src = "./images/svg-icons/header_dropdown_menu_closed.svg";
-            document.getElementById("header_dropdown-menu_icon").style.margin = "5px 0px auto 0px";
-        }
+window.addEventListener("click", function(event) {
+    let x = document.getElementById("toggling_elem");
+    if ( (event.target.matches("#header_dropdown-menu_icon") != true) && (x.classList.contains("showMenu") == true) ) {
+        x.classList.remove("showMenu");
+        document.getElementById("header_dropdown-menu_icon").src = "./images/svg-icons/header_dropdown_menu_closed.svg";
+        document.getElementById("header_dropdown-menu_icon").style.margin = "5px 0px auto 0px";
     }
-}*/
+});
